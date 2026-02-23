40b5be030bcec5238577c8804b6c60fb28925b0f
ankan1096 <104295869+ankan1096@users.noreply.github.com>
2026-02-23 12:28:28 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,13 +2,13 @@
 // Replace these values with your own Firebase project credentials
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyBSm9Vu_cll9DvwLtZmyjDyEp6AB5JAlO8",
+    authDomain: "egypt-trip-2026-e36d9.firebaseapp.com",
+    databaseURL: "https://egypt-trip-2026-e36d9-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "egypt-trip-2026-e36d9",
+    storageBucket: "egypt-trip-2026-e36d9.firebasestorage.app",
+    messagingSenderId: "56759055881",
+    appId: "1:56759055881:web:83d41231fd4c9f23a9aad8"
 };
 
 // Initialize Firebase (will be done in app.js)
